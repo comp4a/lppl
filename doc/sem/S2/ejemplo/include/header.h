1edb030d84952db147b0d8f08539cf4e4dd6727b
edoelas <miego.cvcv@gmail.com>
2019-10-01 13:16:38 +0200

    p1

---
+++ doc/sem/S2/ejemplo/include/header.h
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/*****************************************************************************/
+#ifndef _HEADER_H
+#define _HEADER_H
+
+/************************************* Variables externas definidas en el AL */
+#define TRUE  1
+#define FALSE 0
+
+extern int yylex();
+extern int yylparse();
+
+extern FILE *yyin;
+extern int   yylineno;
+
+/****************************************************** Funciones auxiliares */
+extern void yyerror(const char * msg) ;   /* Tratamiento de errores          */
+
+extern int verbosidad ;
+
+#endif  /* _HEADER_H */
+/*****************************************************************************/
+/*****************************************************************************/
